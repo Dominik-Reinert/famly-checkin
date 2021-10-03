11aa2afdfed89fd21c13fd32a734d9df5ca61f05
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-10-03 10:51:03 +0200

    add the sever data interfaces, some adaptation the infinite scroll lib, the first child componente

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,7 +1,9 @@
 /**@jsx jsx */
 import { jsx } from "@emotion/react";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useLanguageTranslation } from "../i18n";
+import { daycareStore } from "../store/daycare_store";
 import { useStyleContext } from "../style_context/use_style_context";
 import { homePageStyle, homePageSuspendingStyle } from "./home_page_style";
 
@@ -25,11 +27,44 @@ function HomePageFallback(): JSX.Element {
 }
 
 const HomePageSuspending = () => {
-    const styleContext = useStyleContext();
-    const [t] = useLanguageTranslation();
-    return (
-      <div css={homePageSuspendingStyle(styleContext)}>
-        <div className="scrollable-content"></div>
+  const styleContext = useStyleContext();
+  const [t] = useLanguageTranslation();
+  const children = daycareStore.getCurrentDataAdapted().children;
+  const numberOfItemsMultiplier = React.useRef(1);
+  const numberOfItemsToScroll = 20;
+  const numberOfItemsToRender = React.useMemo(
+    () => numberOfItemsToScroll * numberOfItemsMultiplier.current,
+    [children, numberOfItemsToScroll, numberOfItemsMultiplier]
+  );
+  const increaseMultiplier = React.useCallback(
+    () => numberOfItemsMultiplier.current++,
+    [numberOfItemsMultiplier, numberOfItemsMultiplier.current]
+  );
+  return (
+    <div css={homePageSuspendingStyle(styleContext)}>
+      <div className="scrollable-content">
+        <InfiniteScroll
+          dataLength={children.length}
+          next={increaseMultiplier}
+          hasMore={numberOfItemsToRender < children.length}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {children
+            .slice(0, Math.max(numberOfItemsToRender, children.length) - 1)
+            .map((child) => (
+              <Child key={child.name.fullName} />
+            ))}
+        </InfiniteScroll>
       </div>
-    );
-  };
+    </div>
+  );
+};
+
+function Child(props: any): JSX.Element {
+  return <div></div>;
+}
---
+++ src/store/daycare_store.ts
@@ -0,0 +1,45 @@
+import { AbstractStore } from "./abstract_store";
+import { Daycare } from "./daycare_server_interfaces";
+import { ServerData } from "./server_data";
+interface ChildName {
+  fullName: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+
+interface Child {
+  id: string;
+  name: ChildName;
+}
+
+interface AdaptedDaycareData {
+  children: Child[];
+}
+
+interface DaycareData {
+  daycareRoot: ServerData<Daycare.RootObject>;
+}
+
+class DaycareStore extends AbstractStore<DaycareData, AdaptedDaycareData> {
+  adaptData(data: DaycareData): AdaptedDaycareData {
+    return {
+      children: data.daycareRoot
+        .get()
+        .children.map((child) => ({ name: child.name, id: child.childId })),
+    };
+  }
+}
+
+export const daycareStore: DaycareStore = new DaycareStore({
+  daycareRoot: createDaycareRootServerData(),
+});
+
+function createDaycareRootServerData(): ServerData<Daycare.RootObject> {
+  return new ServerData<Daycare.RootObject>({
+    fetch: () =>
+      fetch(
+        "https://tryfamly.co/api/daycare/tablet/group?accessToken=234ffdb8-0889-4be3-b096-97ab1679752c&groupId=11fc220c-ebba-4e55-9346-cd1eed714620&institutionId=fb6c8114-387e-4051-8cf7-4e388a77b673"
+      ).then((result) => result.json()) as Promise<Daycare.RootObject>,
+  });
+}
